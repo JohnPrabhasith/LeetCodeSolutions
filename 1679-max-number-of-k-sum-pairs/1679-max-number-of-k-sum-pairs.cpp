ea887879ee290ae79be40d98750089751188cd83
Lalith Prabhasith Bakthula <johnprabhasith@gmail.com>
2024-11-01 23:40:01 +0530

    Time: 91 ms (65%), Space: 68.9 MB (37.72%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,5 +1,11 @@
 class Solution {
+
 public:
+    Solution() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
     int maxOperations(vector<int>& arr, int k) {
         map<int, int> mp;
         int count = 0;
