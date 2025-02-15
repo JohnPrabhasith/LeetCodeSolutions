0572917c7269bb62baf37a8a25446bab79e066c5
Lalith Prabhasith Bakthula <johnprabhasith@gmail.com>
2025-02-15 13:30:07 +0530

    Time: 3 ms (5.85%), Space: 10.5 MB (69.31%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -11,8 +11,15 @@ private:
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1,-1);
+        vector<int> dp(n+1,0);
 
-        return dfs(n, nums,dp);
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i=2; i<=n; i++) {
+            dp[i] = max((dp[i-2]+nums[i-1]), dp[i-1]);
+        }
+        return dp[n];
+        // return dfs(n, nums,dp);
     }
 };
