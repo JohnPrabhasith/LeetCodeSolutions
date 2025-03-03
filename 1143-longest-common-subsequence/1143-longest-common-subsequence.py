1515e54da0aace87368c90b5d7c4c9adf83780f4
Lalith Prabhasith Bakthula <johnprabhasith@gmail.com>
2025-03-03 17:00:17 +0530

    Time: 470 ms (57.7%), Space: 52 MB (22.64%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -2,9 +2,10 @@ class Solution:
     def longestCommonSubsequence(self, s: str, t: str) -> int:
         m, n = len(s), len(t)
 
-        dp = [[-1]*n for _ in range(m)]
+        # dp = [[-1]*(n) for _ in range(m)]
+        dp = [[0]*(n+1) for _ in range(m+1)]
 
-        def returnAns(i, j, s, t):
+        def recursiveAns(i, j, s, t):
             if i < 0 or j < 0:
                 return 0
 
@@ -12,10 +13,19 @@ class Solution:
                 return dp[i][j]
 
             if s[i] == t[j]:
-                dp[i][j] = 1 + returnAns(i-1, j-1, s, t)
+                dp[i][j] = 1 + recursiveAns(i-1, j-1, s, t)
             else:
-                dp[i][j] = max(returnAns(i-1, j , s, t), returnAns(i, j-1, s, t))
+                dp[i][j] = max(recursiveAns(i-1, j , s, t), recursiveAns(i, j-1, s, t))
 
             return dp[i][j]
         
-        return returnAns(m-1, n-1, s, t)
+        def TabulationAns(m, n, s, t):
+            for i in range(1,m+1):
+                for j in range(1,n+1):
+                    if s[i-1] == t[j-1]:
+                        dp[i][j] = 1 + dp[i-1][j-1]
+                    else:
+                        dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            return dp[m][n]
+        # return recursiveAns(m-1, n-1, s, t)
+        return TabulationAns(m, n, s, t)
