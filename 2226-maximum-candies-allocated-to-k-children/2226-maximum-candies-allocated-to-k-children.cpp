1dda0e5142cbbe9175deafa3ae53dc67ea732b14
Lalith Prabhasith Bakthula <johnprabhasith@gmail.com>
2025-05-18 02:11:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -15,10 +15,10 @@ private:
 public:
     int maximumCandies(vector<int>& candies, long long k) {
         if (arrSum(candies) < k) return 0;
-        if(k == 4686898339) return 1;
-        if(k == 14763043008) return 1;
+        // if(k == 4686898339) return 1;
+        // if(k == 14763043008) return 1;
         int l = 0;        
-        int r = *max_element(candies.begin(), candies.end());
+        int r = arrSum(candies)/k;
         int ans = 0;
 
         while(l < r) {
