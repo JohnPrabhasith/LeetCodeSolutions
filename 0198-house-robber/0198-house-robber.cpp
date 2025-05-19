2e30c7ddd44d3ae220e7c174d908e26c855adc74
Lalith Prabhasith Bakthula <johnprabhasith@gmail.com>
2025-05-19 20:56:41 +0530

    Time: 0 ms (100%), Space: 10.7 MB (22.3%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,25 +1,21 @@
 class Solution {
-private:
-    int dfs(int n, vector<int>& arr, vector<int>& dp) {
-        if (n == 0) return 0;
-        if (n == 1) return arr[0];
-        else if(dp[n] != -1) return dp[n];
-        int pick = arr[n-1] + dfs(n-2, arr,dp);
-        int notPick = dfs(n-1, arr,dp);
-        return dp[n] = max(pick, notPick);
-    }
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n+1,0);
-
+        vector<int> dp((nums.size()) + 1);
+        return rob(nums, dp, nums.size());
+    }
+private:
+    int rob(vector<int>& nums, vector<int>& dp, int n) {
+        // if(n < 0) return 0;
+        // if(dp[n] != -1) return dp[n];
+        // return dp[n] = max(rob(nums, dp, n-2) + nums[n], rob(nums, dp, n-1));
         dp[0] = 0;
         dp[1] = nums[0];
 
-        for(int i=2; i<=n; i++) {
-            dp[i] = max((dp[i-2]+nums[i-1]), dp[i-1]);
+        for(int i=1; i<n; i++) {
+            int val = nums[i];
+            dp[i+1] = max(dp[i], dp[i-1] + val);
         }
         return dp[n];
-        // return dfs(n, nums,dp);
     }
 };
