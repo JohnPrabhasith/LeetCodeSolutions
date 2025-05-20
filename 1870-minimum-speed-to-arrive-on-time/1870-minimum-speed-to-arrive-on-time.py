c92ca4d788d5cec77c996ad671e44eff43e56728
Lalith Prabhasith Bakthula <johnprabhasith@gmail.com>
2025-05-21 01:32:19 +0530

    Time: 2574 ms (5.05%), Space: 28.6 MB (75.84%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+        n: int = len(dist)
+        if hour < n-1:
+            return -1
+        def bin_search() -> int:
+            l = 1
+            r = int(1e7)
+            while(l <= r):
+                mid = (l+r)//2
+                ans = isValid(mid)
+                # if(ans == hour):
+                if ans:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            return l if l <= 1e7 else -1
+
+
+        def isValid(val) -> bool:
+            ans: float = 0
+            for i in range(n):
+                ans += float(dist[i])/val
+                if i < n-1:
+                    ans = ceil(ans)
+                if ans > hour:
+                    return False
+            return ans <= hour
+
+            
+        return bin_search()
