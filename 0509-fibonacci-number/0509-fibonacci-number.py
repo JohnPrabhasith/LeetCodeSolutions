44fbdb50bc3a5c414867bf3b87adec577eeed8f0
Lalith Prabhasith Bakthula <johnprabhasith@gmail.com>
2025-07-07 22:05:44 +0530

    Time: 35 ms (82.23%), Space: 17.6 MB (89.36%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,9 +1,13 @@
 class Solution:
     def fib(self, n: int) -> int:
-        curr, prev = 1, 0
-        ans = 0
-        for i in range(n):
-            ans = prev + curr
-            prev = curr
-            curr = ans
-        return prev
+        if n == 0 or n == 1:
+            return n
+        x = 0
+        y = 1
+        temp = -1
+        for i in range(n-1):
+            temp = x
+            x = y
+            y = temp + y
+        return y
+            
