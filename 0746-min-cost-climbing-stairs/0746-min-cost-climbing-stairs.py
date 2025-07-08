11d632baf08082b60827b86e6a189cef6ae39112
Lalith Prabhasith Bakthula <johnprabhasith@gmail.com>
2025-07-08 17:40:02 +0530

    Time: 0 ms (100%), Space: 17.7 MB (97.67%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -3,13 +3,16 @@ class Solution:
         n = len(cost)
         dp = [-1] * (n + 1)            
 
-        def minCost(n):
-            if n < 0: return 0
-            if n == 0 or n == 1: return cost[n]
-            if dp[n] != -1: return dp[n]
+        # def minCost(n):
+        #     if n < 0: return 0
+        #     if n == 0 or n == 1: return cost[n]
+        #     if dp[n] != -1: return dp[n]
             
-            dp[n] = cost[n] + min(minCost(n-1), minCost(n-2))
-            return dp[n]
+        #     dp[n] = cost[n] + min(minCost(n-1), minCost(n-2))
+        #     return dp[n]
         
-        return min(minCost(n-1), minCost(n-2))
-
+        # return min(minCost(n-1), minCost(n-2))
+        dp[0], dp[1] = cost[0], cost[1]
+        for i in range(2, n):
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+        return min(dp[n-1], dp[n-2])
