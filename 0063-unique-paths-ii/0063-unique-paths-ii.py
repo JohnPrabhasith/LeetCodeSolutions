8f5572bfebfa26bf24d021a0b5b7ddc77f49c842
Lalith Prabhasith Bakthula <johnprabhasith@gmail.com>
2025-07-09 15:46:48 +0530

    Time: 0 ms (100%), Space: 18.1 MB (30.54%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -4,22 +4,42 @@ class Solution:
         if arr[m-1][n-1] != 0:
             return 0
 
-        dp = [[None]*n for _ in range(m)]
+        # dp = [[None]*n for _ in range(m)]
 
-        # for i in range(m-2, -1, -1):
-        #     for j in range(n-2, -1, -1)
+        ## Approach 1: Recursive DP
 
-        def solve(i, j):
-            if i == m-1 and j == n-1:
-                return 1
-            if i >= m or j >= n:
-                return 0
-            if dp[i][j] is not None:
-                return dp[i][j]
-            if arr[i][j] != 1:
-                dp[i][j] = solve(i+1, j) + solve(i, j+1)
-            else:
-                return 0
-            return dp[i][j]
+        # def solve(i, j):
+        #     if i == m-1 and j == n-1:
+        #         return 1
+        #     if i >= m or j >= n:
+        #         return 0
+        #     if dp[i][j] is not None:
+        #         return dp[i][j]
+        #     if arr[i][j] != 1:
+        #         dp[i][j] = solve(i+1, j) + solve(i, j+1)
+        #     else:
+        #         return 0
+        #     return dp[i][j]
 
-        return solve(0, 0)
+        # return solve(0, 0)
+
+        ## Pure DP(Tabulation):
+
+        dp = [[0]*n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if arr[i][j] == 1:
+                    dp[i][j] = 0
+                elif i == 0 and j == 0:
+                    dp[i][j] = 1
+                else:
+                    down = right = 0
+                    if i > 0: 
+                        right = dp[i-1][j]
+                    if j > 0: 
+                        down = dp[i][j-1]
+                    dp[i][j] = right + down
+                
+        return dp[m-1][n-1]
+
+
