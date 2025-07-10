c434201721e9d5fd30a51998485085c9250d65b9
Lalith Prabhasith Bakthula <johnprabhasith@gmail.com>
2025-07-10 23:05:36 +0530

    Time: 3 ms (85.75%), Space: 18.9 MB (75.98%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,14 +1,15 @@
 class Solution:
     def minimumTotal(self, arr: List[List[int]]) -> int:
         m, n = len(arr), len(arr[0])
-        if m == 0:
-            return 0
-        if m == 1:
-            return arr[0][0]
+        # if m == 0:
+        #     return 0
+        # if m == 1:
+        #     return arr[0][0]
 
-        dp = arr[-1][:]
+        # dp = arr[-1][:]
 
         for i in range(m-2, -1, -1):
             for j in range(i+1):
-                dp[j] = arr[i][j] + min(dp[j], dp[j+1])
-        return dp[0]
+                # dp[j] = arr[i][j] + min(dp[j], dp[j+1])
+                arr[i][j] += min(arr[i+1][j], arr[i+1][j+1])
+        return arr[0][0]
