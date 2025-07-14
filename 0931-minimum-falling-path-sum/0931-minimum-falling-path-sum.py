831666d3c4063f70fdf54d654d79dccd51f08075
Lalith Prabhasith Bakthula <johnprabhasith@gmail.com>
2025-07-15 00:44:49 +0530

    Time: 14 ms (92.46%), Space: 18.6 MB (90.29%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -1,15 +1,16 @@
 class Solution:
-    def minFallingPathSum(self, dp: List[List[int]]) -> int:
-        m, n = len(dp), len(dp[0])
-
-        # dp = [[0] * (n) for _ in range(m)]
-        # dp[-1] = dp[-1]
+    def minFallingPathSum(self, arr: List[List[int]]) -> int:
+        m, n = len(arr), len(arr[0])
+        
+        dp = arr[-1][:]
         for i in range(m-2, -1, -1):
+            temp = [0]*n
             for j in range(n):
                 if j == 0:
-                    dp[i][j] += min(dp[i+1][j], dp[i+1][j+1])
+                    temp[j] = arr[i][j] + min(dp[j], dp[j+1])
                 elif j == n-1:
-                    dp[i][j] += min(dp[i+1][j-1], dp[i+1][j])
+                    temp[j] = arr[i][j] + min(dp[j-1], dp[j])
                 else:
-                    dp[i][j] += min(dp[i+1][j-1], dp[i+1][j], dp[i+1][j+1])
-        return min(dp[0])
+                    temp[j] = arr[i][j] + min(dp[j-1], dp[j], dp[j+1])
+            dp = temp
+        return min(dp)
