80d3065c3d8845d52495b929ccd0d22220263e6f
Lalith Prabhasith Bakthula <johnprabhasith@gmail.com>
2025-07-14 23:24:06 +0530

    Time: 45 ms (53.29%), Space: 29 MB (65.31%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -7,7 +7,7 @@ class Solution:
                 nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
         
         for i in range(n):
-            if nums[i] != i + 1:
+            if nums[i] != i +1:
                 return i + 1
         
-        return n + 1
+        return n+1
