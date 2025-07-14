84790e8f8e8808bf2342f1bc5797de495b6ef530
Lalith Prabhasith Bakthula <johnprabhasith@gmail.com>
2025-07-15 00:46:01 +0530

    Time: 11 ms (98.52%), Space: 18.5 MB (97.45%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -12,5 +12,30 @@ class Solution:
                     temp[j] = arr[i][j] + min(dp[j-1], dp[j])
                 else:
                     temp[j] = arr[i][j] + min(dp[j-1], dp[j], dp[j+1])
+            ## Approach 2
+            '''
+            if else:
+                dp[i][j] = arr[i][j] +  min((i+1), (j-1, j, j+1))
+                arr[i][j] += min(arr[i+1][j], .....)
+               ''' 
             dp = temp
         return min(dp)
+
+
+
+
+
+        ##Approach 3 : Recursive
+        # dp = [[-1]*(n+1) for _ in range(m+1)]
+        # def solve(i, j):
+        #     if j < 0 or j >= m: return 1e9
+        #     if i == 0: return arr[0][j]
+        #     if dp[i][j] != -1: return dp[i][j]
+        #     s = arr[i][j] + solve(i-1, j)        ## Straight up
+        #     dl = arr[i][j] + solve(i-1, j-1)     ## Diagonal left
+        #     dr =  arr[i][j] + solve(i-1, j+1)    ## Diagonal Right
+        #     dp[i][j] = min(s, dl, dr)
+        #     return dp[i][j]
+
+        # return min(solve(m-1,j) for j in range(n))
+
