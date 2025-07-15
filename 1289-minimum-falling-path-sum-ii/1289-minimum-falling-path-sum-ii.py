aaedfb3bb5d1ca4a2403da5050fd790cb73229f3
Lalith Prabhasith Bakthula <johnprabhasith@gmail.com>
2025-07-16 00:47:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minFallingPathSum(self, arr: List[List[int]]) -> int:
+        n = len(arr)
+        memo = {}
+
+        def solve(row, col):
+            if col >= n or col < 0:
+                return 10e9+7
+            if row == 0:
+                return arr[row][col]
+            if (row, col) in memo:
+                return memo[(row, col)]
+            res = 10e9 + 7
+            for prev_col in range(n):
+                if col != prev_col:
+                    res = min(res, arr[row][col] + solve(row-1, prev_col))
+            memo[(row, col)] = res
+            return res
+
+        res = 10e9 +7
+        for i in range(n):
+            res = min(res, solve(n-1, i))
+        return res
+
+        
